dd022296a19c0c407d63a8f8c45dcb7c62c32348
RoyalGuard97 <51274711+RoyalGuard97@users.noreply.github.com>
2023-08-21 20:14:39 -0500

    Primer Commmit Yakomo

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,82 @@
 import 'package:flutter/material.dart';
 
+//Constantes de Color
 const Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+const Color ultraRed = Color(0xFFEF3340);
+const Color ultraWhite = Color.fromARGB(255, 243, 238, 238);
 
+//Función Main
 void main() {
   runApp(MyApp());
 }
 
+//Widget Principal
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.light().copyWith(
-        scaffoldBackgroundColor: Colors.white,
+        scaffoldBackgroundColor: ultraWhite
       ),
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: MyWidget(),
+      home: HomeApp(),
+    );
+  }
+}
+
+//Widget Home
+class HomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("UltraBrother M78") ,
+          backgroundColor: ultraRed,
         ),
+        body: UltraWidget(name: "@ULTRAMAN",)
+    );
+  }
+}
+
+//Widget para Cada Ultra
+class UltraWidget extends StatelessWidget {
+  //Variables
+  final String name;
+  //Constructor
+  UltraWidget({required this.name});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //Imagen
+          Image.asset(
+            "assets/ultraman.jpg",
+            width: 300,
+            height: 500,
+          ),
+          //Separacionn
+          const SizedBox(height: 20),
+          //Botón
+          ElevatedButton(
+            //Acción del Botón
+            onPressed: () => print("Desplegar Lista de Kaijus"),
+            child: Text("Registros Kaiju"),
+            //Estilo del Botón
+            style: ElevatedButton.styleFrom(
+              backgroundColor: ultraRed
+            ),
+          ),
+          //Separación
+          const SizedBox(height: 10,),
+          //Texto
+          Text(name)
+        ],
       ),
     );
   }
 }
 
-class MyWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'Hola Mundo',
-      style: Theme.of(context).textTheme.headlineMedium,
-    );
-  }
-}
